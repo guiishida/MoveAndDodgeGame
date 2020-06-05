90428f0b17f5edd64656ca66f76c158dc9f29a31
guiishida <guiishida1357@gmail.com>
2020-06-05 14:18:52 -0300

    Add Animated PacMan and Enemy models from blender

---
+++ scripts/PacManAnimated.gd
@@ -0,0 +1,54 @@
+extends KinematicBody
+
+const GRAVITY = -9.8
+const SPEED = 5
+const ACCELERATION = 3
+const DE_ACCELERATION = 5
+const ZERO = 0
+
+var velocity = Vector3()
+var character
+#var camera
+
+func _ready():
+	character = get_node(".")
+	$Empty.rotate_y(-77)
+
+func _physics_process(delta):
+	#camera = get_node("CameraTarget/Camera").get_global_transform()
+	var is_moving = false
+	
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		is_moving = false
+		velocity.x = ZERO
+	elif Input.is_action_pressed("ui_right"):
+		is_moving = true
+		velocity.x = SPEED	
+	elif Input.is_action_pressed("ui_left"):
+		is_moving = true
+		velocity.x = -SPEED
+	else:
+		is_moving = true
+		velocity.x = lerp(velocity.x, 0, 0.1)
+		
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		is_moving = false
+		velocity.z = ZERO
+	elif Input.is_action_pressed("ui_up"):
+		is_moving = true
+		velocity.z = -SPEED
+	elif Input.is_action_pressed("ui_down"):
+		is_moving = true
+		velocity.z = SPEED
+	else:
+		is_moving = true
+		velocity.z = lerp(velocity.z, 0, 0.1)
+	
+	move_and_slide(velocity)
+	
+	if is_moving:
+		var angles = atan2(velocity.x, velocity.z)
+		var characterRot = character.get_rotation()
+		characterRot.y = angles
+		character.set_rotation(characterRot)
+				
